83528f05e010c55639bc5e3b5c3604fa42d53696
harryob <55142896+harryob@users.noreply.github.com>
2023-05-03 18:10:13 +0100

    ticket channel

--- src/lib/redis/ingestTicket.ts
+++ src/lib/redis/ingestTicket.ts
@@ -2,11 +2,11 @@ import { EmbedBuilder, TextChannel } from 'discord.js';
 import { container } from '@sapphire/framework';
 
 export const ingestTicket = async (message: string, channel: string) => {
-	if (!process.env.MOD_CHANNEL || !channel) return;
+	if (!process.env.TICKET_CHANNEL || !channel) return;
 
 	const { client } = container;
 
-	const channelToUse = client.channels.cache.get(process.env.MOD_CHANNEL);
+	const channelToUse = client.channels.cache.get(process.env.TICKET_CHANNEL);
 	if (!channelToUse || !(channelToUse instanceof TextChannel)) return;
 
 	const data = JSON.parse(message);
