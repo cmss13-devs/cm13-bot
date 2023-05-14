9eafb76bae03977db54e6b85ac15906e635a157b
harryob <55142896+harryob@users.noreply.github.com>
2023-05-14 22:19:32 +0100

    await

--- src/lib/discord/addRoles.ts
+++ src/lib/discord/addRoles.ts
@@ -4,5 +4,5 @@ import type { User } from 'discord.js';
 export const addRoles = async (user: User, roles: Array<string> | null, reason?: string) => {
 	if (!process.env.GUILD || !roles) return;
 
-	container.client.guilds.cache.get(process.env.GUILD)?.members.cache.get(user.id)?.roles.add(roles, reason);
+	await container.client.guilds.cache.get(process.env.GUILD)?.members.cache.get(user.id)?.roles.add(roles, reason);
 };
--- src/lib/discord/removeRole.ts
+++ src/lib/discord/removeRole.ts
@@ -4,5 +4,5 @@ import type { User } from 'discord.js';
 export const removeRole = async (user: User, role: string, reason?: string) => {
 	if (!process.env.GUILD || !role) return;
 
-	container.client.guilds.cache.get(process.env.GUILD)?.members.cache.get(user.id)?.roles.remove(role, reason);
+	await container.client.guilds.cache.get(process.env.GUILD)?.members.cache.get(user.id)?.roles.remove(role, reason);
 };
