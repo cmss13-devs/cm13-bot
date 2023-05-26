2e1674e26be60f87e27ae696257e3e59366e5e65
harryob <55142896+harryob@users.noreply.github.com>
2023-05-26 15:57:11 +0100

    no way

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ container.redisSub = createClient({
 const main = async () => {
 	try {
 		client.logger.info('Logging in.');
-		await client.login(process.env.CM13_DISCORD_BOT_TOKEN);
+		await client.login(process.env.CM13_DISCORD_TOKEN);
 		client.logger.info('Logged in.');
 	} catch (error) {
 		client.logger.fatal(error);
