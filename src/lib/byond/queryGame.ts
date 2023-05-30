b20ab99784735ddbd683f45dce2af93ec1029944
harryob <55142896+harryob@users.noreply.github.com>
2023-05-30 07:27:43 +0100

    lets not

--- src/lib/byond/queryGame.ts
+++ src/lib/byond/queryGame.ts
@@ -1,4 +1,3 @@
-import { container } from '@sapphire/framework';
 import { sendTopic } from 'http2byond';
 
 export interface ByondQuery {
@@ -8,17 +7,11 @@ export interface ByondQuery {
 export const queryDatabase = async (query: string, additional?: object): Promise<any> => {
 	if (!process.env.CM13_BOT_GAME_AUTH_TOKEN || !process.env.CM13_BOT_GAME_SOURCE || !process.env.CM13_BOT_GAME_SERVER_HOST || !process.env.CM13_BOT_GAME_SERVER_PORT || !query) return;
 
-	if(container.processingEnquiry) return setTimeout(await queryDatabase(query, additional), 3000)
-
-	container.processingEnquiry = true
-
 	const data = await sendTopic({
 		host: process.env.CM13_BOT_GAME_SERVER_HOST,
 		port: parseInt(process.env.CM13_BOT_GAME_SERVER_PORT),
 		topic: JSON.stringify({ auth: process.env.CM13_BOT_GAME_AUTH_TOKEN, query: query, source: process.env.CM13_BOT_GAME_SOURCE, ...additional })
 	});
-
-	container.processingEnquiry = false
 
 	if (!data) return;
 
