b6d48f7f946222c497e9d1f19ec34552c4bbeb83
harryob <55142896+harryob@users.noreply.github.com>
2023-06-22 10:31:21 +0100

    more `s

--- src/lib/redis/ingestAccess.ts
+++ src/lib/redis/ingestAccess.ts
@@ -18,7 +18,7 @@ export const ingestAccess = async (message: string, channel: string) => {
 		const emojiToSend = `${data.type === 'login' ? formatEmoji(process.env.CM13_BOT_DISCORD_EMOJI_GREEN) : formatEmoji(process.env.CM13_BOT_DISCORD_EMOJI_RED)}`
 		const sourceString = `${data.key}@${data.source}`
 		const body = `Logged ${data.type === 'login' ? 'in' : 'out'} with ${data.remaining} staff online. (${data.afk} AFK)`
-		channelToUse.send(`${emojiToSend} ${sourceString}: ${body}`)
+		channelToUse.send(`${emojiToSend} \`${sourceString}\`: \`${body}\``)
 		return
 	}
 
